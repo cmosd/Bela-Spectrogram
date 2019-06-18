7fa5fab3c51b4986cf85c73284fc7a2a6633c871
alexandrusoloms <mariussolomes@gmail.com>
2019-06-18 12:21:06 +0100

    adding fft matrix read-wav-file spectrogram .h files

---
+++ inc/FFT.h
@@ -0,0 +1,22 @@
+//
+// Created by mariussolomes on 16/06/19.
+//
+
+#ifndef BELA_SPECTROGRAM_FFT_H
+#define BELA_SPECTROGRAM_FFT_H
+
+#include <complex>
+#include <iostream>
+#include <valarray>
+
+const double PI = 3.141592653589793238460;
+
+typedef std::complex<double> Complex;
+typedef std::valarray<Complex> CArray;
+
+// Cooley–Tukey FFT (in-place, divide-and-conquer)
+// Higher memory requirements and redundancy although more intuitive
+void fft(CArray& x);
+
+
+#endif //BELA_SPECTROGRAM_FFT_H
---
+++ inc/Matrix.h
@@ -0,0 +1,44 @@
+//
+// Created by mariussolomes on 16/06/19.
+//
+
+#ifndef BELA_SPECTROGRAM_MATRIX_H
+#define BELA_SPECTROGRAM_MATRIX_H
+
+#include <iostream>
+#include <complex>
+#include <vector>
+
+#define EPS             2.220446049250313e-16
+/*
+This matrix class is for now only able to hold double*...
+TODO: make this take a template <typename T>.
+*/
+
+class Matrix{
+public:
+    Matrix(int n_rows, int n_cols);
+    ~Matrix() = default;
+
+    void print();
+
+    int rows, cols;
+    std::vector<double> data;
+
+ private:
+    void initMatrix();
+};
+
+/* Matrix functions */
+
+Matrix matrixAddition(Matrix* m1, Matrix* m2);
+Matrix matrixTranspose(Matrix* m);
+Matrix matrixDotProduct(Matrix* m1, Matrix* m2);
+Matrix duplicateMatrix(Matrix* m);
+Matrix append1DMatrices(Matrix* m1, Matrix* m2);
+Matrix arrange(int n_rows, int n_cols, double start, double end, double increment);
+Matrix lookupByPosition(Matrix* m, Matrix* position);
+Matrix matrixReplaceZeros(Matrix* m);
+void saveMatrix(Matrix* m);
+
+#endif //BELA_SPECTROGRAM_MATRIX_H
